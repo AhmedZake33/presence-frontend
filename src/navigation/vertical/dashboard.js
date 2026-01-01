5ec1c1e2584b4cf924605042def5e7acb1abea0a
ahmed zake <ahmed.zake333@gmail.com>
2026-01-01 10:51:26 +0200

    feat: update title and improve accessibility message in index.html

--- src/navigation/vertical/dashboard.js
+++ src/navigation/vertical/dashboard.js
@@ -18,55 +18,55 @@ export default [
   //     },
   //   ],
   // },
-      {
-        title: 'Dashboard',
-        icon: 'HomeIcon',
-        route: 'employee-dashboard',
-        meta: { role: ['employee'] },
-      },
-      {
-        title: 'Dashboard',
-        icon: 'HomeIcon',
-        route: 'attendance-dashboard',
-        meta: { role: ['admin'] },
-      },
-      {
-        title: 'Employees',
-        icon: 'UserIcon',
-        route: 'attendance-employees',
-        meta: { role: ['admin','manager'] },
-      },
-      {
-        title: 'Attendance',
-        icon: 'CalendarIcon',
-        route: 'attendance', 
-        meta: { role: ['employee'] },
-      },
-      {
-        title: 'Create Day Off',
-        icon: 'CalendarIcon',
-        route: 'create-day-off',
-        meta: { role: ['employee'] },
-      },
-      {
-        title: 'Day Off Requests',
-        icon: 'CalendarIcon',
-        route: 'day-off-requests',
-        meta: { role: ['admin','manager'] },
-      },
-      {
-        title: 'Teams',
-        icon: 'UsersIcon',
-        route: 'teams',
-        meta: { role: ['admin'] },
-      },
-      {
-        title: 'settings',
-        icon: 'SettingsIcon',
-        route: 'settings',
-        meta: { role: ['admin'] },
-      },
-      
-      
+  {
+    title: 'Dashboard',
+    icon: 'HomeIcon',
+    route: 'employee-dashboard',
+    meta: { role: ['employee'] },
+  },
+  {
+    title: 'Dashboard',
+    icon: 'HomeIcon',
+    route: 'attendance-dashboard',
+    meta: { role: ['admin'] },
+  },
+  {
+    title: 'Employees',
+    icon: 'UserIcon',
+    route: 'attendance-employees',
+    meta: { role: ['admin', 'manager'] },
+  },
+  // {
+  //   title: 'Attendance',
+  //   icon: 'CalendarIcon',
+  //   route: 'attendance', 
+  //   meta: { role: ['employee'] },
+  // },
+  // {
+  //   title: 'Create Day Off',
+  //   icon: 'CalendarIcon',
+  //   route: 'create-day-off',
+  //   meta: { role: ['employee'] },
+  // },
+  {
+    title: 'Day Off Requests',
+    icon: 'CalendarIcon',
+    route: 'day-off-requests',
+    meta: { role: ['admin', 'manager'] },
+  },
+  {
+    title: 'Teams',
+    icon: 'UsersIcon',
+    route: 'teams',
+    meta: { role: ['admin'] },
+  },
+  {
+    title: 'Settings',
+    icon: 'SettingsIcon',
+    route: 'settings',
+    meta: { role: ['admin'] },
+  },
+
+
 ]
 
